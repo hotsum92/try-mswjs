99f5d15da6521ef47a1170a4a23cbea452ecfcc3
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-10-06 15:37:09 +0900

    feat: setup src directory

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: outputPath
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+  }
+}
